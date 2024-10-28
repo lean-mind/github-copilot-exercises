821da7948acac43674848844ca9dd8ccafdc217a
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-10-28 16:54:04 +0000

    :fire: Remove Edge cases tests

--- StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -47,60 +47,6 @@ public class StringCalculatorTests
 
     public class EdgeCases
     {
-        [Fact]
-        public void Add_EmptyString_ReturnsZero()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("");
-            Assert.Equal("0", result);
-        }
-        
-        [Fact]
-        public void Add_InvalidNewlineSeparator_ReturnsErrorMessage()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("175.2,\n35");
-            Assert.Equal("Number expected but '\\n' found at position 6.", result);
-        }
 
-        [Fact]
-        public void Add_MissingNumberInLastPosition_ReturnsErrorMessage()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("1,3,");
-            Assert.Equal("Number expected but EOF found.", result);
-        }
-
-        [Fact]
-        public void Add_CustomDelimiterWithInvalidSeparator_ReturnsErrorMessage()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("//|\n1|2,3");
-            Assert.Equal("'|' expected but ',' found at position 3.", result);
-        }
-
-        [Fact]
-        public void Add_NegativeNumbers_ReturnsErrorMessage()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("-1,2");
-            Assert.Equal("Negative not allowed : -1", result);
-        }
-
-        [Fact]
-        public void Add_MultipleNegativeNumbers_ReturnsErrorMessage()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("2,-4,-5");
-            Assert.Equal("Negative not allowed : -4, -5", result);
-        }
-
-        [Fact]
-        public void Add_MultipleErrors_ReturnsAllErrorMessages()
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("-1,,2");
-            Assert.Equal("Negative not allowed : -1\nNumber expected but ',' found at position 3.", result);
-        }
     }
 }
